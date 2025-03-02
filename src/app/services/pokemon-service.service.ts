50047444a45c647cba4483e20dbe05eedf6eafde
pablo <stevaopabllo@gmail.com>
2025-03-02 10:55:18 -0300

    services

---
+++ src/app/services/pokemon-service.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PokemonServiceService {
+  url_API:String = 'ng generate service pokemon'
+  constructor() { }
+}
